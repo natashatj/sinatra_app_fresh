66f8fc480f8893c00d6ac0f6db08bb5690fd4db8
Natasha Jadavji <natasha@jadavji.ca>
2016-07-09 00:03:32 +0000

    5 to 8

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,22 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+    erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  @message.save
+  redirect '/messages'
+end
